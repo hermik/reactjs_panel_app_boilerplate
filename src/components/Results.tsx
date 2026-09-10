4bd6687e705767487e3ac5dcfab974a893a8f21f
hermik <mhermanowicz@gmail.com>
2026-09-10 17:29:31 +0200

    little change to json responce

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,17 +1,11 @@
 export default function Results({ results }: { results: any[] }) {
 
-    const items = Array.isArray(results) ? results : []
-    if (items.length === 0) {
-        return (
-            <div>
-                <p>Results will appear here...</p>
-            </div>
-        )
-    }
+  const items = Array.isArray(results) ? results : []
+   
   return (
     <div>
       {items.map((result, index) => (
-        <p key={index}>{result}</p>
+        <p key={index}>{result.title}</p>
       ))}
     </div>
   )
