bee5cf78b71d264131125c9a675d71bae7e8f364
hermik <mhermanowicz@gmail.com>
2026-09-10 16:09:24 +0200

    littla changes in layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 import SidebarToggle from './SidebarToggle'
 import Sidebar from './Sidebar'
 import NavBar from './NavBar';
@@ -8,10 +8,14 @@ export default function Layout() {
     return (
         <div className="layout">
             <header className="main">
-                <h1>My super app</h1>
-                <NavBar />
-                <SidebarToggle />
-                <LogoutButton />
+                <div className="logo"><NavLink to="/">My super app</NavLink></div>
+                <div>
+                    <NavBar />
+                </div>
+                <div>
+                    <SidebarToggle />
+                    <LogoutButton />
+                </div>
             </header>
             <section className="content">
                 <Sidebar /> 
