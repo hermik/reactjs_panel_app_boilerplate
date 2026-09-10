f1430deb4e40b111ad2b95e199ddcad0444212cf
hermik <mhermanowicz@gmail.com>
2026-09-10 16:08:14 +0200

    changes in user store

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -4,13 +4,11 @@ interface UserState {
     name: string
     email: string
     isLogged: boolean
-    accessToken: string | null
-    login: () => void,
     actions: {
-        setName: (name: string) => void,
-        setEmail: (email: string) => void,
-        setAccessToken: (accessToken: string | null) => void,
-        logout: () => void,
+        setName: (name: string) => void
+        setEmail: (email: string) => void
+        login: () => void
+        logout: () => void
     }
 }
 
@@ -18,12 +16,10 @@ export const useUserStore = create<UserState>((set) => ({
     name: '',
     email: '',
     isLogged: false,
-    accessToken: null,
-    login: () => set({ isLogged: true }),
     actions: {
         setName: (name: string) => set({ name }),
         setEmail: (email: string) => set({ email }),
-        setAccessToken: (accessToken: string | null) => set({ accessToken, isLogged: !!accessToken }),
-        logout: () => set({ isLogged: false, accessToken: null }),
+        login: () => set({ isLogged: true }),
+        logout: () => set({ isLogged: false }),
     }
 }))
