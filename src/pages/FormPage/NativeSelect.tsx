75aa7e5645bc4baf758093fb412843a2ad830e7b
hermik <mhermanowicz@gmail.com>
2026-09-17 11:03:47 +0200

    css - > better looking forms. Forms was little unreadable (all white) now its more ux fiendly

--- src/pages/FormPage/NativeSelect.tsx
+++ src/pages/FormPage/NativeSelect.tsx
@@ -11,7 +11,7 @@ function NativeSelect({ className, children, ...props }: React.ComponentProps<'s
     return (
         <select
             className={cn(
-                'h-8 w-full min-w-0 rounded-lg border border-input bg-transparent px-2.5 text-base transition-colors outline-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40',
+                'h-8 w-full min-w-0 rounded-lg border border-input bg-input/15 px-2.5 text-base transition-colors outline-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:border-destructive/50 dark:aria-invalid:ring-destructive/40',
                 className,
             )}
             {...props}
